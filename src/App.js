90bffa86d86dfdf9b1abc14615874b96bd0ef5c2
Navneet719 <kumar.navneet719@gmail.com>
2023-09-19 19:12:11 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from './Components/Navbar'
+import Plans from './Components/Plans'
+import Perks from './Components/Perks';
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+
+} from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/Plans" element={<Plans />} />
+        </Routes>
+        <Routes>
+          <Route path="/Perks" element={<Perks />} />
+        </Routes>
+
+      </div>
+    </Router>
+
+
+
   );
 }
 
